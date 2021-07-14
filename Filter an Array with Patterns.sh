9f973d4f10084a2c43839d4e3d8deb602e7f3cce
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-14 21:37:50 +0530

    Update Filter an Array with Patterns.sh

--- Filter an Array with Patterns.sh
+++ Filter an Array with Patterns.sh
@@ -1,1 +1,13 @@
-echo  ${Array[@]/*[aA]*/}
+while read word; do
+    array=(${array[*]} $word)
+done
+
+for var in ${array[*]}; do
+    if echo $var | grep 'A' > /dev/null ; then
+        continue
+    elif echo $var | grep 'a'> /dev/null; then 
+        continue
+    else
+        echo $var
+    fi
+done
